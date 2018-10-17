818e11eda8a3f72e9bea716343a42b3a00ab9b34
hvekriya <h.vekriya@kainos.com>
2018-10-17 12:13:46 +0100

    Added skeleton vue app

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+    <Nav></Nav>
+  </div>
+</template>
+
+<script>
+import Nav from '@/components/nav'
+export default {
+  name: 'app',
+  components: {
+    Nav: Nav
+  }
+}
+</script>
+
+<style>
+  @import "https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.css";
+  @import "https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.9.0/fullcalendar.min.css";
+  @import "https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.0/animate.min.css";
+  @import "https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.5.2/jquery.fancybox.min.js";
+  @import "./assets/sass/libs/jquery.fb.albumbrowser.css";
+  @import "https://use.fontawesome.com/releases/v5.4.1/css/all.css";
+</style>
+
+<style lang="scss">
+  @import "./assets/sass/main.scss";
+</style>
+
+<link rel="stylesheet" href="public/css/vendor/fullcalendar.min.css">
+<link rel="stylesheet" href="public/css/vendor/jquery.fancybox.css">
+<link rel="stylesheet" href="public/css/vendor/animate.css">
+<link rel="stylesheet" href="public/css/vendor/jquery.fb.albumbrowser.css">
+<link rel="stylesheet" href="public/css/vendor/nanogallery2.min.css"> -->
---
+++ src/components/nav.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <b-navbar type="dark" variant="info" toggleable>
+
+      <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
+    
+      <b-navbar-brand href="/" class="d-sm-none d-lg-none">SKSS Woolwich</b-navbar-brand>
+    
+      <b-collapse is-nav id="nav_collapse">
+        <b-navbar-nav>
+          <b-nav-item href="#">Home</b-nav-item>
+          <b-nav-item-dropdown text="Our Temple">
+            <b-dropdown-item href="/our-temple">Our Temple</b-dropdown-item>
+            <b-dropdown-item href="/history">History</b-dropdown-item>
+            <b-dropdown-item href="/murtis">Murtis</b-dropdown-item>
+            <b-dropdown-item href="/weekly-activities">Weekly Activities</b-dropdown-item>
+            <b-dropdown-item href="/calendar">Calendar</b-dropdown-item>
+            <b-dropdown-item href="/yuvak-mandal">Yuvak Mandal</b-dropdown-item>
+            <b-dropdown-item href="/swaminarayan-education">Swaminarayan Education</b-dropdown-item>
+            <!-- <b-dropdown-item href="/careers">Careers</b-dropdown-item> -->
+          </b-nav-item-dropdown>
+        </b-navbar-nav>
+    
+        <!-- Right aligned nav items -->
+        <b-navbar-nav class="ml-auto">
+          <b-nav-item disabled>Follow us</b-nav-item>
+          <b-nav-item href="https://www.facebook.com/WoolwichTemple/"><i class="fab fa-facebook-f"></i></b-nav-item>
+          <b-nav-item href="https://www.twitter.com/WoolwichTemple/"><i class="fab fa-twitter"></i></b-nav-item>
+          <b-nav-item href="https://www.instagram.com/WoolwichTemple/"><i class="fab fa-instagram"></i></b-nav-item>
+          <b-nav-item href="https://www.youtube.com/woolwichtemple"><i class="fab fa-youtube"></i></b-nav-item>
+          <b-nav-item href="tel:+447437001008"><i class="fab fa-whatsapp"></i></b-nav-item>
+          <b-nav-item href="https://www.snapchat.com/add/woolwichtemple"><i class="fab fa-snapchat"></i></b-nav-item>
+        </b-navbar-nav>
+    
+      </b-collapse>
+    </b-navbar>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Nav'
+  }
+</script>
+
+<style>
+
+</style>
