24ebe762919a8fe961b5978b186690158f50200f
hvekriya <haresh.1994@hotmail.com>
2019-08-17 09:58:10 +0100

    Added nursery url

--- src/router.js
+++ src/router.js
@@ -1,110 +1,116 @@
-import Vue from 'vue'
-import Router from 'vue-router'
+import Vue from "vue";
+import Router from "vue-router";
 
-Vue.use(Router)
+Vue.use(Router);
 
 function loadView(view) {
-  return () => import( /* webpackChunkName: "view-[request]" */ `@/views/${view}.vue`)
+  return () =>
+    import(/* webpackChunkName: "view-[request]" */ `@/views/${view}.vue`);
 }
 
 export default new Router({
-  mode: 'history',
-  routes: [{
-      path: '/',
-      name: 'Home',
-      component: loadView('Home')
+  mode: "history",
+  routes: [
+    {
+      path: "/",
+      name: "Home",
+      component: loadView("Home")
     },
     // Our-temple routes
     {
-      path: '/our-temple/weekly-schedule',
-      name: 'Schedule',
-      component: loadView('our-temple/Schedule')
+      path: "/our-temple/weekly-schedule",
+      name: "Schedule",
+      component: loadView("our-temple/Schedule")
     },
     {
-      path: '/our-temple/calendar',
-      name: 'Calendar',
-      component: loadView('our-temple/Calendar')
+      path: "/our-temple/calendar",
+      name: "Calendar",
+      component: loadView("our-temple/Calendar")
     },
     {
-      path: '/our-temple/swaminarayan-education',
-      name: 'Education',
-      component: loadView('our-temple/Education')
+      path: "/our-temple/swaminarayan-education",
+      name: "Education",
+      component: loadView("our-temple/Education")
     },
-    { // Pages from Prismic
-      path: '/our-temple/:uid',
-      name: 'OurTemple',
-      component: loadView('our-temple/OurTemple')
+    {
+      // Pages from Prismic
+      path: "/our-temple/:uid",
+      name: "OurTemple",
+      component: loadView("our-temple/OurTemple")
     },
     // Our Sampraday
     {
-      path: '/our-sampraday/:tag',
-      name: 'Articles',
-      component: loadView('our-sampraday/Articles')
+      path: "/our-sampraday/:tag",
+      name: "Articles",
+      component: loadView("our-sampraday/Articles")
     },
     {
-      path: '/our-sampraday/articles/:uid',
-      name: 'Article',
-      component: loadView('our-sampraday/Article')
+      path: "/our-sampraday/articles/:uid",
+      name: "Article",
+      component: loadView("our-sampraday/Article")
     },
     // Media
     {
-      path: '/daily-darshan',
-      name: 'DailyDarshan',
-      component: loadView('DailyDarshan')
+      path: "/daily-darshan",
+      name: "DailyDarshan",
+      component: loadView("DailyDarshan")
     },
     // Blog
     {
-      path: '/blog',
-      name: 'Blog',
-      component: loadView('Blog')
+      path: "/blog",
+      name: "Blog",
+      component: loadView("Blog")
     },
     {
-      path: '/post/:uid',
-      name: 'Post',
-      component: loadView('Post')
+      path: "/post/:uid",
+      name: "Post",
+      component: loadView("Post")
     },
     // Wedding Hall
     {
-      path: '/wedding-hall',
-      name: 'WeddingHall',
-      component: loadView('WeddingHall')
+      path: "/wedding-hall",
+      name: "WeddingHall",
+      component: loadView("WeddingHall")
     },
     // Nursery
     {
-      path: '/our-temple/nursery',
-      name: 'Nursery',
-      component: loadView('Nursery')
+      path: "/our-temple/nursery",
+      name: "Nursery",
+      beforeEnter(to, from, next) {
+        // Put the full page url including the protocol http(s) below
+        window.location = "https://balkrishna.org";
+      }
     },
     // Donation
     {
-      path: '/donation',
-      name: 'Donation',
-      component: loadView('Donation')
+      path: "/donation",
+      name: "Donation",
+      component: loadView("Donation")
     },
     // Contact us
     {
-      path: '/contact-us',
-      name: 'ContactUs',
-      component: loadView('ContactUs')
+      path: "/contact-us",
+      name: "ContactUs",
+      component: loadView("ContactUs")
     },
     // Other pages
     {
-      path: '/admin',
+      path: "/admin",
       beforeEnter(to, from, next) {
         // Put the full page url including the protocol http(s) below
-        window.location = 'https://sksswoolwich.prismic.io'
+        window.location = "https://sksswoolwich.prismic.io";
       }
     },
     {
-      path: '/not-found',
-      name: 'not-found',
-      component: loadView('NotFound')
+      path: "/not-found",
+      name: "not-found",
+      component: loadView("NotFound")
     },
     {
-      path: '*',
+      path: "*",
       redirect: {
-        name: 'not-found'
+        name: "not-found"
       }
     }
-  ],
-})
+  ]
+});
