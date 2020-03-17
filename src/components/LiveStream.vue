10a5d03c1eb1784c972481a12b1593717728ee79
Haresh Vekriya <haresh.vekriya@gmail.com>
2020-03-17 21:18:49 +0000

    Live stream modifications

--- src/components/LiveStream.vue
+++ src/components/LiveStream.vue
@@ -1,12 +1,12 @@
 <template>
   <div>
     <template v-for="(slice, index) in fields.slices">
-      <template v-if="slice.slice_type === 'video'">
+      <template v-if="slice.slice_type === 'video' && slice.primary.live_stream_enabled">
         <div class="row">
           <div class="embed-responsive embed-responsive-16by9">
             <iframe
               class="embed-responsive-item"
-              :src="slice.primary.video.embed_url | replace('watch?v=','embed/')"
+              :src="slice.primary.live_stream_link.url"
               allowfullscreen
             ></iframe>
           </div>
