bd9b6c111caa54f60f056f8604623a22465f8f75
Haresh Vekriya <haresh.vekriya@gmail.com>
2021-03-21 22:12:30 +0000

    dd update

--- src/components/DailyDarshan.vue
+++ src/components/DailyDarshan.vue
@@ -3,12 +3,12 @@
     <legend>
       <center>Daily Darshan</center>
     </legend>
-    <ul id="dailydarshan">
-      <template v-for="(dd, index) in dailydarshan">
-        <li :data-thumb="dd.media_url" :data-src="dd.media_url">
-          <img :src="dd.media_url" style="width:100%" :alt="dd.caption | replace()" />
+    <ul id="dailydarshan" v-for="(dd, index) in dailydarshan">
+      <template v-for="(img, index) in dd.media_url">
+        <li :data-thumb="img" :data-src="img">
+          <img :src="img" style="width: 100%" />
           <div class="dd-caption">
-            <span>{{ dd.caption | truncate() }}</span>
+            <span>{{ dd.lastUpdatedDate }}</span>
           </div>
         </li>
       </template>
@@ -38,9 +38,27 @@ export default {
           });
         });
     },
+    getDD() {
+      axios.get("http://dd.swaminarayan.faith/api").then((response) => {
+        console.log(response);
+        this.dailydarshan = response.data.data
+          .filter((id) => {
+            return id.id == "woolwich";
+          })
+          .map((item) => {
+            return {
+              media_url: item.lastUpdated.images.map((item) => {
+                return `https://dd.swaminarayan.faith/api${item}`;
+              }),
+              lastUpdatedDate: item.lastUpdated.date,
+            };
+          });
+      });
+    },
   },
   created() {
-    this.getInsta();
+    // this.getInsta(); //Disabled as fb graph api app needs approval
+    this.getDD();
   },
   filters: {
     replace: function (value) {
@@ -56,10 +74,11 @@ export default {
       useCSS: true,
       cssEasing: "ease", //'cubic-bezier(0.25, 0, 0.25, 1)',//
       easing: "linear", //'for jquery animation',////
-      speed: 600, //ms'
+      speed: 900, //ms'
       auto: true,
       slideEndAnimation: true,
       pause: 2000,
+      pager: false,
       onSliderLoad: function (el) {
         el.lightGallery({
           selector: "#dailydarshan .lslide",
