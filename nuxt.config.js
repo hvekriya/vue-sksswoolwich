a3e232706fbfe37c704ead2211e1e8a3722c972d
Haresh Vekriya <haresh.vekriya@gmail.com>
2022-03-25 16:10:54 +0000

    GA4

--- nuxt.config.js
+++ nuxt.config.js
@@ -139,9 +139,14 @@ export default {
   },
 
   googleAnalytics: {
-    id: process.env.GA
+    id: process.env.GA, // Use as fallback if no runtime config is provided
   },
-
+  publicRuntimeConfig: {
+    googleAnalytics: {
+      id: process.env.GA
+    }
+  },
+  
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
