ba308a79a1d544f6314ccec71bdc9360f9bf13f7
Haresh Vekriya <haresh.vekriya@gmail.com>
2022-05-08 15:43:57 +0100

    Added textarea instead of input

--- pages/admin-dash/Announcement.vue
+++ pages/admin-dash/Announcement.vue
@@ -13,7 +13,7 @@
       </div>
       <div class="form-group">
         <label for="description">Description</label>
-        <input
+        <textarea
           type="text"
           class="form-control"
           id="description"
