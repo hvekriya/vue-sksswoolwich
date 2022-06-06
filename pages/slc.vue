cb5f0f5be412723cd002f6b61d9a105bff0fcce7
Haresh Vekriya <haresh.vekriya@gmail.com>
2022-06-06 18:17:13 +0100

    SLC table size

--- pages/slc.vue
+++ pages/slc.vue
@@ -11,15 +11,15 @@
       <h1>SLC Tables</h1>
     </header>
     <div class="row">
-      <iframe
-        width="600"
-        height="500"
-        frameborder="0"
-        style="overflow: hidden"
-        scrolling="no"
-        src="https://onedrive.live.com/embed?resid=455BD0026BA8B44E%211721&authkey=%21AHtwCofYHzwfAig&em=2&wdAllowInteractivity=False&wdHideGridlines=True&wdHideHeaders=True&wdInConfigurator=True&wdInConfigurator=True&edesNext=false&ejss=false"
-      >
-      </iframe>
+      <div class="iframeWrapper">
+        <iframe
+          frameborder="0"
+          style="overflow: hidden"
+          scrolling="no"
+          src="https://onedrive.live.com/embed?resid=455BD0026BA8B44E%211721&authkey=%21AHtwCofYHzwfAig&em=2&wdAllowInteractivity=False&wdHideGridlines=True&wdHideHeaders=True&wdInConfigurator=True&wdInConfigurator=True&edesNext=false&ejss=false"
+        >
+        </iframe>
+      </div>
     </div>
   </div>
 </template>
@@ -30,7 +30,14 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-iframe {
-  overflow: hidden;
+// .iframeWrapper {
+//   position: relative;
+//   padding-bottom: 56.25%; /* 16:9 */
+//   padding-top: 25px;
+//   height: 0;
+// }
+.wrapper iframe {
+  width: 100%;
+  height: 450px;
 }
 </style>
