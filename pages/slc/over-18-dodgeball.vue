87f04e549e90a88d658283a3d1143e4bc8df65a8
Haresh Vekriya <haresh.vekriya@gmail.com>
2022-06-10 12:53:57 +0100

    Sports in mind donation

--- pages/slc/over-18-dodgeball.vue
+++ pages/slc/over-18-dodgeball.vue
@@ -3,41 +3,45 @@
     <header class="page-header">
       <ol class="breadcrumb">
         <li>
-          <NuxtLink to="/slc"
-            ><span class="glyphicon glyphicon-chevron-left"></span> Back to all
+          <NuxtLink to="/slc"><span class="glyphicon glyphicon-chevron-left"></span> Back to all
             tables
           </NuxtLink>
         </li>
       </ol>
-      <img
-        src="../../assets/img/slc-banner-web.png"
-        alt="Swaminarayan League Cup"
-        copyright="Woolwich Temple"
-        class="img-responsive"
-      />
+      <div class="bs-calltoaction bs-calltoaction-primary">
+        <div class="row">
+          <div class="col-md-4 col-lg-4 col-sm-12 cta-contents">
+            <h1 class="cta-title">Sports in mind</h1>
+          </div>
+          <div class="col-md-4 col-lg-4 col-sm-12">
+            <p>Help us reach our target of £2,500!
+              We can’t do it without your help!
+            </p>
+          </div>
+          <div class="col-md-4 cta-button">
+            <a href="https://www.justgiving.com/fundraising/swaminarayan-league-cup?utm_source=Sharethis&utm_medium=fundraising&utm_content=swaminarayan-league-cup&utm_campaign=pfp-email&utm_term=2f933652736c413f9181df015e95e9c1."
+              class="btn btn-lg btn-block btn-primary" target="_blank">DONATE NOW</a>
+          </div>
+        </div>
+        <div class="row"></div>
+      </div>
 
       <h1>Over 18 Dodgeball</h1>
     </header>
     <div class="row">
       <h3>Group A</h3>
       <div class="iframeWrapper">
-        <iframe
-          frameborder="0"
-          style="overflow: hidden"
-          scrolling="no"
-          src="https://onedrive.live.com/embed?resid=455BD0026BA8B44E%211721&authkey=%21AHtwCofYHzwfAig&em=2&wdAllowInteractivity=False&Item=Over18DodgeGrpA&wdHideGridlines=True&wdInConfigurator=True&wdInConfigurator=True&edesNext=false&ejss=false"
-        ></iframe>
+        <iframe frameborder="0" style="overflow: hidden" scrolling="no"
+          src="https://onedrive.live.com/embed?resid=455BD0026BA8B44E%211721&authkey=%21AHtwCofYHzwfAig&em=2&wdAllowInteractivity=False&Item=Over18DodgeGrpA&wdHideGridlines=True&wdInConfigurator=True&wdInConfigurator=True&edesNext=false&ejss=false"></iframe>
       </div>
       <h3>Group B</h3>
       <div class="iframeWrapper">
-        <iframe
-          frameborder="0"
-          style="overflow: hidden"
-          scrolling="no"
-          src="https://onedrive.live.com/embed?resid=455BD0026BA8B44E%211721&authkey=%21AHtwCofYHzwfAig&em=2&wdAllowInteractivity=False&Item=Over18DodgeGrpB&wdHideGridlines=True&wdInConfigurator=True&wdInConfigurator=True&edesNext=false&ejss=false"
-        ></iframe>
+        <iframe frameborder="0" style="overflow: hidden" scrolling="no"
+          src="https://onedrive.live.com/embed?resid=455BD0026BA8B44E%211721&authkey=%21AHtwCofYHzwfAig&em=2&wdAllowInteractivity=False&Item=Over18DodgeGrpB&wdHideGridlines=True&wdInConfigurator=True&wdInConfigurator=True&edesNext=false&ejss=false"></iframe>
       </div>
     </div>
+    <img src="../../assets/img/slc-banner-web.png" alt="Swaminarayan League Cup" copyright="Woolwich Temple"
+      class="img-responsive" />
   </div>
 </template>
 
@@ -51,5 +55,114 @@ export default {
 .wrapper iframe {
   width: 100%;
   height: 250px;
+}
+
+.page-header {
+  padding-bottom: 9px;
+  margin: 0px;
+  border-bottom: 1px solid #eee;
+}
+
+.bs-calltoaction {
+  position: relative;
+  width: auto;
+  padding: 15px 25px;
+  margin-bottom: 10px;
+}
+
+.bs-calltoaction>.row {
+  display: table;
+  width: calc(100% + 30px);
+}
+
+.bs-calltoaction>.row>[class^="col-"],
+.bs-calltoaction>.row>[class*=" col-"] {
+  float: none;
+  display: table-cell;
+  vertical-align: middle;
+}
+
+.cta-contents {
+  padding-top: 10px;
+  padding-bottom: 10px;
+}
+
+.cta-title {
+  margin: 0 auto 15px;
+  padding: 0;
+}
+
+.cta-desc {
+  padding: 0;
+}
+
+.cta-desc p:last-child {
+  margin-bottom: 0;
+}
+
+.cta-button {
+  padding: 20px;
+}
+
+@media (max-width: 991px) {
+  .bs-calltoaction>.row {
+    display: block;
+    width: auto;
+  }
+
+  .bs-calltoaction>.row>[class^="col-"],
+  .bs-calltoaction>.row>[class*=" col-"] {
+    float: none;
+    display: block;
+    vertical-align: middle;
+    position: relative;
+  }
+
+  .cta-contents {
+    text-align: center;
+  }
+}
+
+.bs-calltoaction.bs-calltoaction-default {
+  color: #333;
+  background-color: #fff;
+  border-color: #ccc;
+}
+
+.bs-calltoaction.bs-calltoaction-primary {
+  color: #17172f;
+  background-color: #12d1ff;
+}
+
+.bs-calltoaction.bs-calltoaction-info {
+  color: #fff;
+  background-color: #5bc0de;
+  border-color: #46b8da;
+}
+
+.bs-calltoaction.bs-calltoaction-success {
+  color: #fff;
+  background-color: #5cb85c;
+  border-color: #4cae4c;
+}
+
+.bs-calltoaction.bs-calltoaction-warning {
+  color: #fff;
+  background-color: #f0ad4e;
+  border-color: #eea236;
+}
+
+.bs-calltoaction.bs-calltoaction-danger {
+  color: #fff;
+  background-color: #d9534f;
+  border-color: #d43f3a;
+}
+
+.bs-calltoaction.bs-calltoaction-primary .cta-button .btn,
+.bs-calltoaction.bs-calltoaction-info .cta-button .btn,
+.bs-calltoaction.bs-calltoaction-success .cta-button .btn,
+.bs-calltoaction.bs-calltoaction-warning .cta-button .btn,
+.bs-calltoaction.bs-calltoaction-danger .cta-button .btn {
+  border-color: #fff;
 }
 </style>
