cb52ab8e161b83771e6bf9b623fafc10afcdc5eb
Haresh Vekriya <haresh.vekriya@gmail.com>
2023-01-06 10:15:15 +0000

    remove static folder

--- static/js/main.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-  if (/Android|webOS|iPhone|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
-    $('.topnav .navbar-header').removeClass('animated fadeInDown');
-    $('.navbar-collapse').removeClass('animated fadeInDown');
-    $('.whatsapp').attr('href', 'tel:+447437001008');
-  } else {
-    $('.whatsapp').attr('href', '/messaging.html');
-  }
-})
